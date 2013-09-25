480b5eb435646c583ec563c075167c7b208b2bf9
Ovidiu Drumia <ovidiu.drumia@gmail.com>
2013-09-25 18:21:11 +0300

    Initial commit.

---
+++ src/main/java/com/cegeka/nocturne/godgame/Creature.java
@@ -0,0 +1,9 @@
+package com.cegeka.nocturne.godgame;
+
+/**
+ * User: ovidiud
+ * Date: 25.09.2013
+ * Time: 17:41
+ */
+public interface Creature {
+}
---
+++ src/main/java/com/cegeka/nocturne/godgame/World.java
@@ -0,0 +1,35 @@
+package com.cegeka.nocturne.godgame;
+
+public class World {
+    private final int size;
+    private Creature[][] cells = null;
+    private int daysCounter;
+
+    public World(int i) {
+        if(i <= 0) {
+            throw new IllegalArgumentException("Size should be bigger than 0.");
+        }
+        this.size = i;
+        cells = new Creature[i][i];
+    }
+
+    public double getCellCount() {
+        return this.size * this.size;
+    }
+
+    public void setCell(Creature creature, int x, int y) {
+        cells[x][y] = creature;
+    }
+
+    public Creature getCell(int x, int y) {
+        return cells[x][y];
+    }
+
+    public void passTheDay() {
+        this.daysCounter++;
+    }
+
+    public int getAge() {
+        return daysCounter;
+    }
+}
