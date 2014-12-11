d8c27820b9867468b14498d2c9143c6242ba81db
Andrei Petcu <andrei.petcu@cegeka.com>
2014-12-11 22:54:39 +0200

    refactoring

--- src/test/java/com/cegeka/nocturne/godgame/GrassGeneratorTest.java
+++ src/test/java/com/cegeka/nocturne/godgame/GrassGeneratorTest.java
@@ -45,16 +45,48 @@ public class GrassGeneratorTest {
 	}
 
 	@Test
-	public void havingDays7_generateOneGrass() {
+	public void having7Days_generateOneGrass() {
 		for (int i = 0; i < world.getSize(); i++) {
 			for (int j = 0; j < world.getSize(); j++) {
 				world.setCell(dummyCreature, i, j);
 			}
 		}
 		world.setCell(null, 2, 3);
-		for (int i = 0; i < 7; i++) {
+		for (int i = 0; i < 6; i++) {
 			world.dayPassed();
 		}
+		assertThat(world.getCell(2, 3)).isNull();
+		
+		world.dayPassed();
 		assertThat(world.getCell(2, 3)).isInstanceOf(GrassCreature.class);
+				
 	}
+	
+	@Test
+	public void having7Days_generateExactlyOneGrass() {
+		assertThat(GrassGenerator.countUnocuppiedCells(world)).isEqualTo(25);
+		
+		for (int i = 0; i < 6; i++) {
+			world.dayPassed();
+		}
+		assertThat(GrassGenerator.countUnocuppiedCells(world)).isEqualTo(25);
+		world.dayPassed();
+		
+		assertThat(GrassGenerator.countUnocuppiedCells(world)).isEqualTo(24);						
+	}
+	
+	@Test
+	public void having14Days_generateExactlyOneGrass() {
+		assertThat(GrassGenerator.countUnocuppiedCells(world)).isEqualTo(25);
+		
+		for (int i = 0; i < 13; i++) {
+			world.dayPassed();
+		}
+		
+		assertThat(GrassGenerator.countUnocuppiedCells(world)).isEqualTo(24);
+		
+		world.dayPassed();		
+		assertThat(GrassGenerator.countUnocuppiedCells(world)).isEqualTo(23);						
+	}
+	
 }
