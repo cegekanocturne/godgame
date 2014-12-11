0f16630cd7460276034145960e7ed3ff51b04104
Emilian Losneanu <losneanu@gmail.com>
2014-12-11 22:49:10 +0200

    More work in the integration thingie.

---
+++ demo.bat
@@ -0,0 +1,1 @@
+/c/java/jdk1.8/bin/java -cp target/godgame-0.0.1-SNAPSHOT.jar  com.cegeka.nocturne.godgame.DemoTest
--- src/main/java/com/cegeka/nocturne/godgame/DemoTest.java
+++ src/main/java/com/cegeka/nocturne/godgame/DemoTest.java
@@ -1,9 +1,19 @@
 package com.cegeka.nocturne.godgame;
 
+import java.io.IOException;
+
 import com.cegeka.nocturne.godgame.time.Timer;
 import com.cegeka.nocturne.godgame.time.TimerListener;
 
 public class DemoTest {
+	public void pauseGame() {
+		
+	}
+	
+	public void resumeGame() {
+		
+	}
+	
 	public void doItAll() {
 		World world=new World(5);
 		
@@ -17,6 +27,26 @@ public class DemoTest {
 			@Override
 			public void dayPassed() {
 				displayer.display();
+				
+				try {
+					char c = (char) System.in.read();
+					switch (c) {
+					case 'P':
+					case 'p':
+						pauseGame();
+						
+						break;
+					case 'r':
+					case 'R':
+						resumeGame();
+						break;
+					
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
 			}
 		});
 		
