b347fb8eb1c96245ebc33da8335a04448ceab889
Andrei Petcu <andrei.petcu@cegeka.com>
2014-12-11 22:00:10 +0200

    added pair\

--- src/main/java/com/cegeka/nocturne/godgame/GrassGenerator.java
+++ src/main/java/com/cegeka/nocturne/godgame/GrassGenerator.java
@@ -1,5 +1,10 @@
 package com.cegeka.nocturne.godgame;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 
 public class GrassGenerator {
 	public synchronized boolean generateAndAddToWorld (int age, World world) {
@@ -21,5 +26,28 @@ public class GrassGenerator {
 		}
 		
 		return count;
+	}
+
+	public IntPair generateUnoccupiedPosition(World world) {
+		int emptyCount = countUnocuppiedCells(world);
+		if (emptyCount == 0) {
+			return null;
+		}
+		
+		int randomChoice = 1 + (new Random()).nextInt(emptyCount);
+		
+		int count = 0;
+		for ( int i = 0; i < world.getSize(); i++) {
+			for (int j = 0; j < world.getSize(); j++) {
+				if (world.getCell(i, j) == null) {
+					count++;
+					if (count == randomChoice) {
+						return new IntPair(i, j);
+					}
+				}
+			}
+		}
+		
+		return null;
 	}
 }
